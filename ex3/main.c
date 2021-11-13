94f58072121d525a7cece60f8739e95a0ce0cc88
1shaked <shaked1hen@gmail.com>
2021-11-13 21:18:57 +0200

    Initial Commit

---
+++ ex3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ex3
+//
+//  Created by shaked chen on 13/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
