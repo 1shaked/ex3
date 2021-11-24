1e4537d31ba494abc95335d60fabfea81e65c73a
1shaked <shaked1hen@gmail.com>
2021-11-24 11:09:15 +0200

    all comments and fix minor prints

--- ex3/main.c
+++ ex3/main.c
@@ -1,22 +1,20 @@
-//
-//  main.c
-//  ex3
-//
-//  Created by shaked chen on 13/11/2021.
-//
+/******************************************
+* Shaked Chen
+* 207253220
+* 83-120-01
+* Ex 2
+******************************************/
 
 #include <stdio.h>
-// #include <ctype.h>
 #include <math.h>
 
 #define AMOUNT_PER_PLAYER_REMOVED 10000
 char ToUpper(char v);
-int IsCommandValid(int v);
 int IsNumberValidFromStatus(int v);
 int AmountToBank(int totalPlayers, int playersRemoved, int bank , int charity);
 int AmountToCharity(int totalPlayers, int playersRemoved, int bank, int charity);
-void PrintRepete(int amount, char letter);
-void PrintSqure(int size);
+void PrintRepeate(int amount, char letter);
+void PrintSquare(int size);
 void PrintStart(int size);
 void PrintTriangle(int size);
 int GetMersenneNumber(int limit, int startAt, int includeLimit);
@@ -25,23 +23,19 @@ int GetMersenneNumber(int limit, int startAt, int includeLimit);
 int main(int argc, const char * argv[]) {
     // get first letter of first and last name
     printf("Hello, what is the first letter of your name?\n");
-   
     char firstName;
     char lastName;
-    
     scanf(" %c", &firstName);
     scanf(" %c", &lastName);
     // conveting to upper case
     firstName = ToUpper(firstName);
     lastName = ToUpper(lastName);
-    
     printf("Hello %c. %c, welcome to Squid Game!\n", firstName, lastName);
     // starting the game
     printf("Choose 1 for Statues, 2 for Dalgona, 3 for Gganbu, 4 for Bridge, -1 for exit\n");
     int userInput = 0;
     scanf("%d", &userInput);
     printf("user input is %d\n", userInput);
-    // printf("%d", Inputs[userInput]);
     enum Inputs {Statues = 1,Dalgona = 2,Gganbu = 3,Bridge = 4,Exist=-1};
     enum Inputs inputUser = userInput;
     int playersNumber = 456;
@@ -70,10 +64,6 @@ int main(int argc, const char * argv[]) {
                 }
             }
             playersNumber = playersNumber - totalToRemove;
-            // the amount to go to charity and bank to move it to be wiht a function
-            tempCharity = charity;
-            charity = AmountToCharity(playersNumber, totalToRemove, bank, charity);
-            bank = AmountToBank(playersNumber, totalToRemove, bank, tempCharity);
         }
         else if (inputUser == Dalgona) {
             printf("Dalgona\n");
@@ -89,7 +79,7 @@ int main(int argc, const char * argv[]) {
             }
             switch (shape) {
                 case 1:
-                    PrintSqure(size);
+                    PrintSquare(size);
                     break;
                 case 2:
                     PrintTriangle(size);
@@ -102,19 +92,13 @@ int main(int argc, const char * argv[]) {
             }
             int minNumberToRemove = size / 2 + 1;
             int remainPlayers = 0;
-            // TODO: fix the filtring
             if (playersNumber >= minNumberToRemove) {
                 int upperNumberToRemove = size * size;
-                // int amountToRemove;
                 // calc the amount of players to remove
                 if (upperNumberToRemove <= playersNumber) remainPlayers = upperNumberToRemove - minNumberToRemove + 1;
                 else remainPlayers = playersNumber - minNumberToRemove + 1;
                 totalToRemove = playersNumber - remainPlayers;
                 playersNumber = playersNumber - totalToRemove;
-                // the amount to go to charity and bank to move it to be wiht a function
-                tempCharity = charity;
-                charity = AmountToCharity(playersNumber, totalToRemove, bank, charity);
-                bank = AmountToBank(playersNumber, totalToRemove, bank, tempCharity);
             }
         }
         else if (inputUser == Gganbu) {
@@ -127,10 +111,6 @@ int main(int argc, const char * argv[]) {
             int playersRemin = playersNumber / deathNumber;
             totalToRemove = playersNumber - playersRemin;
             playersNumber = playersRemin;
-            // the amount to go to charity and bank to move it to be wiht a function
-            tempCharity = charity;
-            charity = AmountToCharity(playersNumber, totalToRemove, bank, charity);
-            bank = AmountToBank(playersNumber, totalToRemove, bank, tempCharity);
         }
         else if (inputUser == Bridge) {
             printf("Bridge!\n");
@@ -146,37 +126,48 @@ int main(int argc, const char * argv[]) {
             }
             // when there less players then the number inputed
             if (maxNumber > playersNumber) maxNumber = playersNumber;
-            int playersToRemove, playersRemain = 0;
+            int playersRemain = 0;
+            
             if (minNumber > playersNumber) {
-                playersToRemove = playersNumber;
-            } else {
+                totalToRemove = playersNumber;
+                printf("None\n");
+            }
+            else {
                 // in order to calculate the amount of players remin
                 int minMersenneNumber = GetMersenneNumber(minNumber, 0 , 1);
                 int maxMersenneNumber = GetMersenneNumber(maxNumber, minMersenneNumber , 0);
                 // calculate how many players remain and handle the edge case when there is no Mersenne in range
                 if (minMersenneNumber > maxMersenneNumber) {
-                    printf("None");
+                    printf("None\n");
                     playersRemain = 0;
                 } else playersRemain = maxMersenneNumber - minMersenneNumber + 1;
-                playersToRemove = playersNumber - playersRemain;
+                totalToRemove = playersNumber - playersRemain;
             }
             // the amount to go to charity and bank to move it to be wiht a function
-            playersNumber = playersNumber - playersToRemove;
-            tempCharity = charity;
-            charity = AmountToCharity(playersNumber, playersToRemove, bank, charity);
-            bank = AmountToBank(playersNumber, playersToRemove, bank, tempCharity);
+            playersNumber = playersNumber - totalToRemove;
         }
+        // the amount to go to charity and bank
+        tempCharity = charity;
+        charity = AmountToCharity(playersNumber, totalToRemove, bank, charity);
+        bank = AmountToBank(playersNumber, totalToRemove, bank, tempCharity);
         printf("There are %d survival(s), and they got %d each. The amount of the rest is: %d$.\n", playersNumber, bank, charity);
+        // reget the user input
         printf("Choose 1 for Statues, 2 for Dalgona, 3 for Gganbu, 4 for Bridge, -1 for exit\n");
         if (playersNumber == 0) break;
         userInput = 0;
         scanf("%d", &userInput);
         inputUser = userInput;
     }
-    printf("Thank for playing");
+    printf("Thank you, %c. %c, have a nice day, the World is Fair!\n", firstName, lastName);
     return 0;
 }
 
+/*************************************************************************
+Function name: ToUpper
+Input: char
+Output: char
+The function operation: take in char and make hime upper case a to A
+************************************************************************/
 char ToUpper(char v) {
     int letterAscii = v;
     if (letterAscii > 96 && letterAscii < 123) {
@@ -185,101 +176,157 @@ char ToUpper(char v) {
     }
     return letterAscii;
 }
-int IsCommandValid(int v) {
-    return v > -2 && v < 5 && v != 0;
-}
+/*************************************************************************
+Function name: IsNumberValidFromStatus
+Input: int
+Output: int
+The function operation: return true/1 if the inputed number is valid for the Status
+************************************************************************/
 int IsNumberValidFromStatus(int v) {
     return v > 0 && v < 10;
 }
+/*************************************************************************
+Function name: AmountToBank
+Input: int totalPlayers, int playersRemoved, int bank, int charity
+Output: int
+The function operation: Calc the amount of moeny the bank will have
+************************************************************************/
 int AmountToBank(int totalPlayers, int playersRemoved, int bank, int charity) {
     // calc the amount of moeny before the removal;
     int totalBankMoeny = ( (playersRemoved + totalPlayers) * bank) + charity;
     int totalMoeny = playersRemoved * AMOUNT_PER_PLAYER_REMOVED + totalBankMoeny;
     return totalMoeny / totalPlayers ;
 }
-
+/*************************************************************************
+Function name: AmountToCharity
+Input: int totalPlayers, int playersRemoved, int bank, int charity
+Output: int
+The function operation: Calc the amount of moeny the Charity will have
+************************************************************************/
 int AmountToCharity(int totalPlayers, int playersRemoved, int bank, int charity) {
     int totalBankMoeny = (playersRemoved + totalPlayers) * bank + charity;
     int totalMoeny = playersRemoved * AMOUNT_PER_PLAYER_REMOVED + totalBankMoeny;
     return totalMoeny % totalPlayers ;
 }
-void PrintRepete(int amount, char letter) {
+/*************************************************************************
+Function name: PrintRepeate
+Input: int amount, char letter
+Output: void
+The function operation: Print the inputed letter the amount of time inpputed in the same line
+************************************************************************/
+void PrintRepeate(int amount, char letter) {
     int i = 0;
     for (;i < amount; i++) {
         printf("%c",letter);
     }
 }
-void PrintSqure(int size) {
+/*************************************************************************
+Function name: PrintSquare
+Input: int size
+Output: void
+The function operation: Print the Squre for the giving size
+************************************************************************/
+void PrintSquare(int size) {
     int i = 0;
     char const letter = 'O';
     for (; i < size; i++) {
         
         if( i == 0 || i == size - 1) {
             // printing the line when is the first or the last row
-            PrintRepete(size, letter);
+            PrintRepeate(size, letter);
         }
         else {
             printf("%c", letter);
-            PrintRepete(size-2, '-');
+            PrintRepeate(size-2, '-');
             printf("%c", letter);
         }
         printf("\n");
     }
 }
+/*************************************************************************
+Function name: PrintTriangle
+Input: int size
+Output: void
+The function operation: Print the Triangle for the giving size
+************************************************************************/
 void PrintTriangle(int size) {
     int totalLoopSize = (size * 2) + 1;
     const char sideLetter = 'O';
     int i = 0;
     for (; i < totalLoopSize; i++) {
-        if (i == totalLoopSize - 1 || i == 0) {
+        if (i == totalLoopSize - 1 || i < 2) {
             // using i in order to avoid nested loop and conditions
-            PrintRepete(i + 1, sideLetter);
+            PrintRepeate(i + 1, sideLetter);
         } else {
             // calculating how much -- needed for the Triangle
-            int fillerAmount = i - 2;
+            int fillerAmount = i - 1;
             printf("%c", sideLetter);
-            PrintRepete(fillerAmount, '-');
+            PrintRepeate(fillerAmount, '-');
             printf("%c", sideLetter);
         }
         printf("\n");
     }
 }
+/*************************************************************************
+Function name: StarLine
+Input: int size
+Output: void
+The function operation: Print the Star line base in the row type
+************************************************************************/
 void PrintStarLine(int size, int row) {
     if (row == 1) {
-        PrintRepete(size - 1, '-');
+        // full of -- and one O
+        PrintRepeate(size - 1, '-');
         printf("O");
-        PrintRepete(size - 1, '-');
+        PrintRepeate(size - 1, '-');
     } else if (row == 2) {
-        PrintRepete(size - 2, '-');
+        // the expected print is -----o-o-----
+        PrintRepeate(size - 2, '-');
         printf("O-O");
-        PrintRepete(size - 2, '-');
+        PrintRepeate(size - 2, '-');
     } else if (row == 4) {
+        // expected -o-------o-
         printf("-O");
-        PrintRepete((size * 2) - 5, '-');
+        PrintRepeate((size * 2) - 5, '-');
         printf("O-");
     }
     else if (row == 5) {
+        // expected --o-------o--
         printf("--O");
-        PrintRepete((size * 2) - 7, '-');
+        PrintRepeate((size * 2) - 7, '-');
         printf("O--");
     }
     printf("\n");
     
 }
+/*************************************************************************
+Function name: PrintStart
+Input: int size
+Output: void
+The function operation: Print the Star shape
+************************************************************************/
 void PrintStart(int size) {
     int rowLen = (size * 2) - 1;
     PrintStarLine(size, 1);
     PrintStarLine(size, 2);
-    PrintRepete(rowLen, 'O');
+    PrintRepeate(rowLen, 'O');
     printf("\n");
     PrintStarLine(size, 4);
     PrintStarLine(size, 5);
     PrintStarLine(size, 4);
-    PrintRepete(rowLen, 'O');
+    PrintRepeate(rowLen, 'O');
     printf("\n");
     PrintStarLine(size, 2);
     PrintStarLine(size, 1);
 }
+/*************************************************************************
+Function name: GetMersenneNumber
+Input: int limit, int startAt, int includeLimit
+Output: void
+The function operation: This function will calculate the power of the number needed for the Mersenn number. StartAt param is in order to improve preformance and lower the loop count
+Example: limit = 10 startAt = 0 includeLimit = 0 will return 3 because the highest Mersenn number is 7
+Example: limit = 33 startAt = 2 includeLimit = 1 will return 6  because the highest Mersenn included in this limit is 63.
+************************************************************************/
 int GetMersenneNumber(int limit, int startAt, int includeLimit) {
     int n = startAt;
     while (pow(2, n) - 1 < limit) {
